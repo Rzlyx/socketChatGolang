14395f86530d984c7150b298e4c901c598e6f082
yclchuxue <3136165538@qq.com>
2023-05-31 16:14:04 +0800

    login fix

--- dao/mysql/User.go
+++ dao/mysql/User.go
@@ -55,8 +55,8 @@ func Register(p *PO.User) error {
 	return err
 }
 
-func Login(username string) (err error, p1 *PO.User) {
-	p1 = new(PO.User)
+func Login(username string) (err error, p1 *PO.UserPO) {
+	p1 = new(PO.UserPO)
 	sqlStr := "select * from user where username = ?"
 	err = DB.Get(p1, sqlStr, username)
 	return err, p1
