81fb2b6761650a49741bf656310db91425a7dc77
yclchuxue <3136165538@qq.com>
2023-06-03 14:45:37 +0800

    群管理-邀请-接口

--- model/VO/response/group_resp.go
+++ model/VO/response/group_resp.go
@@ -18,15 +18,23 @@ type GroupInfo struct {
 
 // 加入群聊信息,仅展示 群名和头像
 type GroupJoin struct {
-	GroupID   int64  `json:"group_id"`
+	GroupID   int64  `json:"group_id,string"`
 	GroupName string `json:"group_name"`
 	Avatar    string `json:"avatar"`
 }
 
 // 加群申请
 type GroupJoinApply struct {
-	ApplyID   int64  `json:"apply_id"`
-	Applicant int64  `json:"applicant"`
-	TargetID  int64  `json:"target_id"`
+	ApplyID   int64  `json:"apply_id,string"`
+	Applicant int64  `json:"applicant,string"`
+	TargetID  int64  `json:"target_id,string"`
 	Reason    string `json:"reason"`
+}
+
+// 邀请加群申请
+type InviteGroupInfo struct {
+	ApplyID   int64 `json:"apply_id,string"`
+	Applicant int64 `json:"applicant,string"`  // 发起邀请人
+	TargetID  int64 `json:"target_id,string"`  // 群ID
+	InvitedID int64 `json:"invited_id,string"` // 被邀请人
 }
